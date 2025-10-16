4b5bbeb3d486db8b69bd9389ed6387c188e893da
abhi-garg-dev <abhikambalwal8118@gmail.com>
2025-10-16 10:24:03 +0530

    project id changed

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "xc2rjtqw",
+  projectId: "a1k7b5ws",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
